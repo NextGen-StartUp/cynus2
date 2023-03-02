184ab60722b0cee838ad3ffd205be05cf9a696a5
Alexander <alexander.nerz@web.de>
2023-03-02 11:09:30 +0100

    remove Analytics frim _App.js

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
         <MyCookieConsent />
         
           <Component {...pageProps} />
-          <Analytics />
+  
         </Layout>
       </DataContextProvider>
     </ChakraProvider>
