f4bb1383b8dd51e11a61e50f37f20aca09746b1b
Alex <alexander.nerz@web.de>
2024-02-29 15:44:37 +0100

    update 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,25 @@
+import Link from "next/link";
+
 function NotFound() {
   return (
     <div className="full-height2 align-left">
-      <div className="div-box full-height">
+      <div className="div-box">
+        <div className="spacer-samll"></div>
+        <div className="spacer-large"></div>
+        <div className="spacer-large"></div>
         <div>
           <h1>404 | Page Not Found</h1>
         </div>
-        <div className="spacer-small"></div>
+        <div className="spacer-medium"></div>
+        <div className="full-height2 align-left">
+          <Link className="" data-link-alt="Landing Page" href="/#LandingPage">
+            <h5>Go back</h5>
+          </Link>
+        </div>
+        <div className="spacer-medium"></div>
+        <div className="spacer-large"></div>
+        <div className="spacer-large"></div>
+        <div className="spacer-large"></div>
       </div>
     </div>
   );
