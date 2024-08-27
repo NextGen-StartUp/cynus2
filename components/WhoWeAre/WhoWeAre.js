00a635dc7b4b904e9f70c8d7d1c4162acaddc834
Alex <alexander.nerz@web.de>
2024-08-27 17:23:13 +0200

    updated style

--- components/WhoWeAre/WhoWeAre.js
+++ components/WhoWeAre/WhoWeAre.js
@@ -6,6 +6,7 @@ export default function WhoWeAre({}) {
   return (
     <section id="WhoWeAre" className="OverlappEffect">
       <div className="full-height background-dark padding-top">
+      <div className="spacer-large"></div>
         <div className="grid-container2 flex-center ">
           <div className="about-info">
             <div className="grid-line full-height padding-about">
@@ -47,61 +48,6 @@ export default function WhoWeAre({}) {
                   of supporters and partners.
                 </h2>
               </FadeInWhenVisible>
-              {/* <FadeInWhenVisible>
-                <div className="spacer-medium"></div>
-                <FadeInWhenVisible>
-                  <h1 className="text-center">The founders</h1>
-                </FadeInWhenVisible>
-                <div className="spacer-small"></div>
-                <FadeInWhenVisible>
-                
-                  <h2 style={{ color: "#ffffff7a" }}>Daniel:</h2>
-
-                  <h2>
-                    Daniel completes the skillset of our founding team and brings
-                  in the economic perspective, essential to validate which
-                  product pass we should follow and how we can build a
-                  sustainable business. Daniel holds a bachelor&apos;s degree in
-                  industrial engineering and a master&apos;s degree in
-                  international business, specialized in entrepreneurship and
-                  SME management.
-                  </h2>
-                </FadeInWhenVisible>
-              </FadeInWhenVisible>
-              <div className="spacer-small"></div>
-              <FadeInWhenVisible>
-                <h2 style={{ color: "#ffffff7a" }}>Delon:</h2>
-
-                <h2>
-                  with his many years of experience in the field of hardware
-                  development and the leadership of a team of 10 people is
-                  responsible for the implementation of the hardware and the
-                  company&apos;s goals.
-                </h2>
-              </FadeInWhenVisible>
-              <div className="spacer-small"></div>
-              <FadeInWhenVisible>
-                <h2 style={{ color: "#ffffff7a" }}>Konstantin:</h2>
-
-                <h2>
-                  Konstantin is currently finalizing his PhD in Micro System
-                    Technologies. Still in the early stages of his career
-                    Konstantin&apos;s interest in Software Development has led
-                    him to develop outstanding software solutions on several
-                    occasions. In combination with his understanding of hardware
-                    Konstantin helps us bring our products to life.
-                </h2>
-              </FadeInWhenVisible>
-              <div className="spacer-small"></div>
-              <FadeInWhenVisible>
-                <h2 style={{ color: "#ffffff7a" }}>Julian:</h2>
-
-                <h2>
-                  Is responsible for the implementation of the applications on
-                  the computer side with his mathematical studies and his many
-                  years of experience in programming.
-                </h2>
-              </FadeInWhenVisible> */}
             </div>
           </div>
           <FadeInWhenVisible>
